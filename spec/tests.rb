8b428180b5278e6fcc9769576439de11de8e62bc
Andrew DuBrock <adubrock@comcast.net>
2013-11-16 11:11:21 -0800

    second test written and failed

--- spec/tests.rb
+++ spec/tests.rb
@@ -6,3 +6,10 @@ describe 'raw_customers.txt exists' do
     File.exists?('../raw_customers.txt').should == true
   end
 end
+
+describe 'should be able to open .txt file and write new file.' do
+
+  it "should open 'raw_customers.txt' and save to 'customers.csv'" do
+    File.exists?('../customers.csv').should == true
+  end
+end
