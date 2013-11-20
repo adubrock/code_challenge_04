63021e3d40b3bb2bc405e77b4f530e212d4ec87c
Andrew DuBrock <adubrock@comcast.net>
2013-11-20 14:59:08 -0800

    New first test. This is really the first one now, Al

--- analyze_tests.rb
+++ analyze_tests.rb
@@ -5,11 +5,7 @@ require './analyze.rb'
 
 describe 'analyze.rb' do
 
-  it "should create the file ('histogram.txt')" do
-    File.exists?('histogram.txt').should == true
-  end
-
-  it "should gather the first word from each line of ('raw_customers.txt')" do
-    histogram.should be_and_instance_of(Array) == true
+  it "should gather prefixes from each line of ('raw_customers.txt')" do
+    File.read('./histogram.txt').should include 'Mr.'
   end
 end
