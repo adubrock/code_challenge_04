e1c507900c8e96c4d3a3c97c9e1d617bb60b04d0
Andrew DuBrock <adubrock@comcast.net>
2013-11-20 16:13:13 -0800

    Another groovy test added, yeehaw

--- analyze_tests.rb
+++ analyze_tests.rb
@@ -1,11 +1,15 @@
 # Encoding:utf-8
 
 require 'rspec'
-require './analyze.rb'
+# require './analyze.rb'
 
 describe 'analyze.rb' do
 
   it "should gather prefixes from each line of ('raw_customers.txt')" do
     File.read('./histogram.txt').should include 'Mr.'
+  end
+
+  it "output should match ('groovy_test_file.txt')" do
+    File.read('./histogram.txt').should == File.read('./groovy_test_file.txt')
   end
 end
