71eacb7672b00118ace7b2ae896b976d57c054f9
Andrew DuBrock <adubrock@comcast.net>
2013-11-22 11:32:45 -0800

    First step done

--- analyze.rb
+++ analyze.rb
@@ -1,30 +1,14 @@
 require 'pry'
+require 'optparse'
 
-histohash = {"Mr." => 1}
+histohash = Hash.new(0)
 File.open('raw_customers.txt', 'r').each_line do |customer|
-  $first = customer.match /^\S{1,}/
-  $first = $first.to_s.split(" ")
-   
-   if histohash.include?($first) == false
-      File.open('histogram.txt', 'r+').each_line do |prefixes|
-      names = prefixes.split(" ")
-      names.each {|name| frequencies[name] +=1}
-      frequencies = frequencies.sort { |a, b| b }
-      frequencies.reverse!
-      frequencies.each { |name, frequency| puts name + " " + frequency.to_s}
-    end
-      else histohash["#{$first}"] +=1
-        puts histohash
-    end
- end
+  first = customer.match /^\S{1,}/
+  first = first.to_s.strip
 
-  #   prefix.puts line
-  # end
-
-
-# end
-
-  # File.open('customers.csv', 'w+') do |csv|
-  #   line = infile.gets
-  #   csv.puts line
-  # end
+  histohash.include?("#{first}") ? histohash["#{first}"] += 1 :
+                                   histohash["#{first}"] = 1
+end
+frequencies = histohash.sort_by { |a, b| b }
+frequencies.reverse!
+frequencies.each { |name, frequency| puts name + ' ' + frequency.to_s }
