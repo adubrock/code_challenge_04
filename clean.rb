41534e1c54de63b84fdee1ed0d9126217b1f7422
Andrew DuBrock <adubrock@comcast.net>
2013-11-25 13:12:30 -0800

    tried test, didn't work. got clean.rb working

--- clean.rb
+++ clean.rb
@@ -1,8 +1,70 @@
  # Encoding:utf-8
  # Clean.rb file
 require 'csv'
+require 'pry'
+#   arr = IO.readlines('raw_customers.txt')
+# File.open('customers.csv', 'w+') do |csv|
+#   csv.puts arr
+# end
 
-  arr = IO.readlines('raw_customers.txt')
-File.open('customers.csv', 'w+') do |csv|
-  csv.puts arr
+# The following regex will pull the phone numbers and whitespace
+# in front of the phone numbers:
+# /\s*\d*\.*-*\(*\d*\)*\d*-*\(*\d*.{12}$/
+
+prefixes = []
+suffixes = []
+
+File.open('prefix_words.txt').each_line do |prefix|
+  prefixes << prefix.to_s.strip
 end
+
+File.open('suffix_words.txt').each_line do |suffix|
+  suffixes << suffix.to_s.strip
+end
+
+puts "prefix, first_name, middle, last_name, suffix, phone_number, phone_extension"
+
+File.open('raw_customers.txt', 'r').each_line do |customer|
+  # phone = customer.match(/\s*\d*\.*-*\(*\d*\)*\d*-*\(*\d*.{12}$/)
+  #         .to_s.strip
+  name = customer.gsub(/\s*\d*\.*-*\(*\d*\)*\d*-*\(*\d*.{12}$/, '')
+         .to_s.strip
+  names = name.split(" ")
+  if suffixes.include?(names.last)
+    suffix = names.last
+    names.pop
+  else
+    suffix = ''
+  end
+  if prefixes.include?(names.first)
+    prefix = names.first
+    names.delete_at(0)
+  else
+    prefix = ''
+  end
+  last = names.last
+  names.pop
+  first = names[0]
+  names.length == 2 ? middle = names.last : middle = ''
+  phone = customer.scan(/\d+/)
+  if phone.length == 5
+      phone_number = "1.#{phone[1]}.#{phone[2]}.#{phone[3]}"
+      phone_extension = "#{phone[4]}"
+  elsif phone.length == 4
+    if phone[0] == 1
+      phone_number = "1.#{phone[1]}.#{phone[2]}.#{phone[3]}"
+      phone_extension = ''
+    else
+      phone_number = "1.#{phone[0]}.#{phone[1]}.#{phone[2]}"
+      phone_extension = "#{phone[3]}"
+    end
+  elsif phone.length == 3
+    phone_number = "1.#{phone[0]}.#{phone[1]}.#{phone[2]}"
+    phone_extension = ''
+  else
+    puts 'Invalid phone number!'
+  end
+puts "#{prefix},#{first},#{middle},#{last},#{suffix},#{phone_number},#{phone_extension}"
+end
+
+
--- tests.rb
+++ tests.rb
@@ -1,17 +1,37 @@
 # Encoding:utf-8
 
 require 'rspec'
-require './clean.rb'
+# require './clean.rb'
 
-describe 'raw_customers.txt exists' do
-
-  it "should exist ('raw_customers.txt')" do
-    File.exists?('raw_customers.txt').should == true
+def create_files
+  in_file = File.open('./groovy_data.txt', 'w') do |x|
+    expected_file = File.open('./expected_names.txt', 'w')
+    expected_file.puts "#{x}"
   end
 end
-describe 'should be able to open .txt file and write new file' do
 
-  it "should open 'raw_customers.txt' and save to 'customers.csv'" do
-    File.exists?('customers.csv').should == true
+def delete_files
+  FileUtils.rm%('./expected_names.txt')
+end
+# describe 'raw_customers.txt exists' do
+
+#   it "should exist ('raw_customers.txt')" do
+#     File.exists?('raw_customers.txt').should == true
+#   end
+# end
+
+# describe 'should be able to open .txt file and write new file' do
+
+#   it "should open 'raw_customers.txt' and save to 'customers.csv'" do
+#     File.exists?('customers.csv').should == true
+#   end
+# end
+
+describe 'get full_name should output the full name' do
+
+  it "should open 'groovy_data.txt' capture each name and save to 'customers.csv" do
+    create_files
+    expect(FileUtils.identical?'./expected_names.txt','./names.txt').to not be_empty
+    delete_files
   end
 end
